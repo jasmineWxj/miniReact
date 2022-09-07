8d7f5d54c829e86a68b37b0c3a4379dfb1a6fab9
wxjing46@163.com <wxjing46@163.com>
2022-09-07 22:47:00 +0800

    aaaa

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    111
     <App />
   </React.StrictMode>
 );
